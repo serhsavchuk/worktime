c8021f97db07593738816729a162b10849b36730
Serhii Savchuk <serhii.savchuk.dev@gmail.com>
2021-10-18 01:21:53 +0300

    Deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/worktime/'
+    base: 'https://github.com/serhsavchuk/worktime/dist/'
   },
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
